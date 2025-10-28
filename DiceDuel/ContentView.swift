78c43c04e7833fd086d57ac453be9bf3c41e98b6
Aiden <aidenbak9@gmail.com>
2025-10-28 10:39:05 -0500

    added dice

--- DiceDuel/ContentView.swift
+++ DiceDuel/ContentView.swift
@@ -6,19 +6,40 @@
 //
 
 import SwiftUI
-
 struct ContentView: View {
+@State private var randomValue = 0
+@State private var rotation = 0.0
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Dice Roll")
+                .font(.title)
+                .padding()
+            Image("pips \(randomValue)")
+                .resizable()
+                .frame(width: 200, height: 200, alignment: .center)
+                .rotationEffect(.degrees(rotation))
+                .rotation3DEffect(.degrees(rotation), axis: (x: 1, y: 1, z: 0))
+                .padding()
+                .onTapGesture {
+                    chooseRandom(times: 3)
+                    withAnimation(.interpolatingSpring(stiffness: 10, damping: 2)) {
+                        rotation += 360
+                    }
+                }
+            Spacer()
         }
-        .padding()
+        }
+        func chooseRandom(times:Int) {
+            if times > 0 {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                    randomValue = Int.random(in: 1...6)
+                    chooseRandom(times: times - 1)
+                }
+            }
+        }
     }
-}
-
-#Preview {
-    ContentView()
-}
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
+    }
