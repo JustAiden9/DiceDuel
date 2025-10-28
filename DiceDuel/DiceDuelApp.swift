43934723d89b3070cd059f79ae5e1be964fa36e1
Aiden <aidenbak9@gmail.com>
2025-10-28 10:31:46 -0500

    Initial Commit

---
+++ DiceDuel/DiceDuelApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceDuelApp.swift
+//  DiceDuel
+//
+//  Created by Aiden Baker on 10/28/25.
+//
+
+import SwiftUI
+
+@main
+struct DiceDuelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DiceDuelTests/DiceDuelTests.swift
@@ -0,0 +1,17 @@
+//
+//  DiceDuelTests.swift
+//  DiceDuelTests
+//
+//  Created by Aiden Baker on 10/28/25.
+//
+
+import Testing
+@testable import DiceDuel
+
+struct DiceDuelTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
