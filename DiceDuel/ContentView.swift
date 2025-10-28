ebda4fbd19f92e89efad6faf56bf0b26c48b730d
Aiden <aidenbak9@gmail.com>
2025-10-28 10:39:38 -0500

    added two dice seperate from each other

--- DiceDuel/ContentView.swift
+++ DiceDuel/ContentView.swift
@@ -7,39 +7,64 @@
 
 import SwiftUI
 struct ContentView: View {
-@State private var randomValue = 0
-@State private var rotation = 0.0
+    @State private var randomValue1 = 1
+    @State private var randomValue2 = 1
+    @State private var rotation1 = 0.0
+    @State private var rotation2 = 0.0
+    
     var body: some View {
         VStack {
             Text("Dice Roll")
                 .font(.title)
                 .padding()
-            Image("pips \(randomValue)")
-                .resizable()
-                .frame(width: 200, height: 200, alignment: .center)
-                .rotationEffect(.degrees(rotation))
-                .rotation3DEffect(.degrees(rotation), axis: (x: 1, y: 1, z: 0))
-                .padding()
-                .onTapGesture {
-                    chooseRandom(times: 3)
-                    withAnimation(.interpolatingSpring(stiffness: 10, damping: 2)) {
-                        rotation += 360
+            HStack(spacing: 40) {
+                Image("pips \(randomValue1)")
+                    .resizable()
+                    .frame(width: 150, height: 150, alignment: .center)
+                    .rotationEffect(.degrees(rotation1))
+                    .rotation3DEffect(.degrees(rotation1), axis: (x: 1, y: 1, z: 0))
+                    .onTapGesture {
+                        chooseRandom1(times: 3)
+                        withAnimation(.interpolatingSpring(stiffness: 10, damping: 2)) {
+                            rotation1 += 360
+                        }
                     }
-                }
+                Image("pips \(randomValue2)")
+                    .resizable()
+                    .frame(width: 150, height: 150, alignment: .center)
+                    .rotationEffect(.degrees(rotation2))
+                    .rotation3DEffect(.degrees(rotation2), axis: (x: 1, y: 1, z: 0))
+                    .onTapGesture {
+                        chooseRandom2(times: 3)
+                        withAnimation(.interpolatingSpring(stiffness: 10, damping: 2)) {
+                            rotation2 += 360
+                        }
+                    }
+            }
             Spacer()
         }
-        }
-        func chooseRandom(times:Int) {
-            if times > 0 {
-                DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
-                    randomValue = Int.random(in: 1...6)
-                    chooseRandom(times: times - 1)
-                }
+    }
+    
+    func chooseRandom1(times: Int) {
+        if times > 0 {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                randomValue1 = Int.random(in: 1...6)
+                chooseRandom1(times: times - 1)
             }
         }
     }
-    struct ContentView_Previews: PreviewProvider {
-        static var previews: some View {
-            ContentView()
+    
+    func chooseRandom2(times: Int) {
+        if times > 0 {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                randomValue2 = Int.random(in: 1...6)
+                chooseRandom2(times: times - 1)
+            }
         }
     }
+}
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
